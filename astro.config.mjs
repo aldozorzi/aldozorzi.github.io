ac83af0abdf93670da105adf4ca44a1053eec4ce
Aldo Zorzi <aldo.zorzi@gmail.com>
2026-09-09 12:05:08 +0200

    sitemap build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ldazrz.com',
   base: '',
+  integrations: [sitemap()],
   redirects: {
     '/projects': '/articles',
     '/projects/understanding-dwarf-star': '/articles/understanding-dwarf-star',
